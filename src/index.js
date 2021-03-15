8113b01d50a2cbc0f3e75d972b798a4d273d1672
nastyabodnar <bodnar.post@gmail.com>
2021-03-16 00:04:01 +0600

    refactor: refactor code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function check(str, bracketsConfig) {
   for (let i = 0; i < bracketsConfig.length; i += 1) { 
     let stringWithBrackets = bracketsConfig[i].join('');
-    .
+    
     if (str.includes(stringWithBrackets)) {
         str = str.replace(stringWithBrackets, '');
         i = -1;
