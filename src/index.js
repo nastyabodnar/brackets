4ebd7cf1090ee08b17f4bbbc0f92c93f72434858
nastyabodnar <bodnar.post@gmail.com>
2021-03-16 00:00:41 +0600

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  for (let i = 0; i < bracketsConfig.length; i += 1) { 
+    let stringWithBrackets = bracketsConfig[i].join('');
+    .
+    if (str.includes(stringWithBrackets)) {
+        str = str.replace(stringWithBrackets, '');
+        i = -1;
+     }
+   }
+    return str.length === 0;
+};
